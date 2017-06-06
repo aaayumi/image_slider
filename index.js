3c9ad5500fec94f9ba0022190265d548f2fd19fd
Ayumi Saito <ayumisaito27@gmail.com>
2017-06-06 23:19:46 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-$(document).ready(function(){
- // The :gt() selector selects elements with an index number higher than a specified number.
- $(".slider img:gt(0)").hide();
- 
-$(".next").click(function(){
-  $(".slider :first-child").fadeOut()
-   .next('img').fadeIn()
-   .end().appendTo('.slider');
-  $(".circle").html("number");
-}) 
+var stopButton = false;
+var slideID = null;
+var mySlide;
 
- });
+$(document).ready(function(){
+    startSlide();
+    
+    function startSlide(){
+    clearInterval(mySlide);
+    $('.slider img:gt(0)').hide();
+    mySlide = setInterval(function(){
+      $('.slider :first-child').fadeOut(300)
+         .next('img').fadeIn()
+         .end().appendTo('.slider');
+    }, 
+      3000); 
+    
+    }
+
+    $("#stop").click(function(){
+        clearInterval(mySlide);
+
+    });
+    $(".start").click(function(){
+        
+            
+        startSlide();
+            
+          });
+                });               
